64978b62df68424ffdb9999cd36e60f6da97a7ea
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-18 14:09:21 +0530

    Implement `FromStr` trait for RGB
    RGB structs can now directly be parsed by reading hex-colors or rgb values as strings

--- src/colors.rs
+++ src/colors.rs
@@ -1,6 +1,8 @@
 use std::fmt::Display;
+use std::str::FromStr;
 
 /// Holds the RGB values of a color
+#[derive(Debug, PartialEq)]
 pub struct RGB<T>(pub T, pub T, pub T);
 
 pub trait RGBColor<T> {
@@ -55,5 +57,125 @@ pub fn interpolate_linear_gradient(start: &RGB<u8>, end: &RGB<u8>, factor: f32) 
     let r = start.r() as f32 + factor * delta.r();
     let g = start.g() as f32 + factor * delta.g();
     let b = start.b() as f32 + factor * delta.b();
-    RGB(r as u8, g as u8, b as u8)
+    RGB(r.round() as u8, g.round() as u8, b.round() as u8)
+}
+
+// -----------
+// PARSE COLOR
+// -----------
+
+#[derive(Debug, PartialEq)]
+pub enum ParseColorError {
+    InvalidFormat,
+    InvalidValues,
+}
+
+impl FromStr for RGB<u8> {
+    type Err = ParseColorError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.starts_with("#") && s.len() == 7 {
+            RGB::from_hex_str(s)
+        } else if s.to_lowercase().starts_with("rgb") {
+            RGB::from_rgb_str(s)
+        } else {
+            Err(ParseColorError::InvalidFormat)
+        }
+    }
+}
+
+impl RGB<u8> {
+    fn from_hex_str(s: &str) -> Result<Self, ParseColorError> {
+        let r = u8::from_str_radix(&s[1..3], 16).map_err(|_| ParseColorError::InvalidValues)?;
+        let g = u8::from_str_radix(&s[3..5], 16).map_err(|_| ParseColorError::InvalidValues)?;
+        let b = u8::from_str_radix(&s[5..7], 16).map_err(|_| ParseColorError::InvalidValues)?;
+        Ok(Self(r, g, b))
+    }
+
+    fn from_rgb_str(s: &str) -> Result<Self, ParseColorError> {
+        let parts: Vec<&str> = s.split(',').map(|part| part.trim()).collect();
+        if parts.len() == 3 {
+            if let (Ok(r), Ok(g), Ok(b)) = (parts[0].parse(), parts[1].parse(), parts[2].parse()) {
+                return Ok(Self(r, g, b));
+            } else {
+                return Err(ParseColorError::InvalidFormat);
+            }
+        } else {
+            Err(ParseColorError::InvalidFormat)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rgb_ansi_code() {
+        let color = RGB(255, 0, 0);
+        assert_eq!(color.ansi_code(), "\x1b[38;2;255;0;0m");
+
+        let color = RGB(0, 255, 0);
+        assert_eq!(color.ansi_code(), "\x1b[38;2;0;255;0m");
+
+        let color = RGB(0, 0, 255);
+        assert_eq!(color.ansi_code(), "\x1b[38;2;0;0;255m");
+    }
+
+    #[test]
+    fn test_calculate_delta() {
+        let start = RGB(255, 0, 0);
+        let end = RGB(0, 0, 255);
+        let delta = calculate_delta(&start, &end);
+        assert_eq!(delta, RGB(-255.0, 0.0, 255.0));
+
+        let start = RGB(0, 255, 0);
+        let end = RGB(0, 0, 255);
+        let delta = calculate_delta(&start, &end);
+        assert_eq!(delta, RGB(0.0, -255.0, 255.0));
+    }
+
+    #[test]
+    fn test_interpolate_linear_gradient() {
+        let start = RGB(255, 0, 0);
+        let end = RGB(0, 0, 255);
+        let mid = interpolate_linear_gradient(&start, &end, 0.5);
+        assert_eq!(mid, RGB(128, 0, 128));
+
+        let start = RGB(0, 255, 0);
+        let end = RGB(0, 0, 255);
+        let mid = interpolate_linear_gradient(&start, &end, 0.5);
+        assert_eq!(mid, RGB(0, 128, 128));
+    }
+
+    #[test]
+    fn test_rgb_from_hex_str() {
+        assert_eq!(RGB::from_hex_str("#000000"), Ok(RGB(0, 0, 0)));
+        assert_eq!(RGB::from_hex_str("#FFFFFF"), Ok(RGB(255, 255, 255)));
+        assert_eq!(RGB::from_hex_str("#FF0000"), Ok(RGB(255, 0, 0)));
+        assert_eq!(RGB::from_hex_str("#00FF00"), Ok(RGB(0, 255, 0)));
+        assert_eq!(RGB::from_hex_str("#0000FF"), Ok(RGB(0, 0, 255)));
+        assert_eq!(
+            RGB::from_str("#GGGGGG"),
+            Err(ParseColorError::InvalidValues)
+        );
+    }
+
+    #[test]
+    fn test_rgb_from_rgb_str() {
+        assert_eq!(RGB::from_rgb_str("0,0,0"), Ok(RGB(0, 0, 0)));
+        assert_eq!(RGB::from_rgb_str("255,255,255"), Ok(RGB(255, 255, 255)));
+        assert_eq!(RGB::from_rgb_str("255,0,0"), Ok(RGB(255, 0, 0)));
+        assert_eq!(RGB::from_rgb_str("0,255,0"), Ok(RGB(0, 255, 0)));
+        assert_eq!(RGB::from_rgb_str("0,0,255"), Ok(RGB(0, 0, 255)));
+        assert_eq!(RGB::from_rgb_str("124, 64, 39"), Ok(RGB(124, 64, 39)));
+        assert_eq!(
+            RGB::from_str("255,255"),
+            Err(ParseColorError::InvalidFormat)
+        );
+        assert_eq!(
+            RGB::from_str("255,GGG,0"),
+            Err(ParseColorError::InvalidFormat)
+        );
+    }
 }
