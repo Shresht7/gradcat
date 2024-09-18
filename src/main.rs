42fa7575ff34a494c65934e2498f689d8059d69d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-18 08:09:38 +0530

    Setup `struct` for command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+/// Command-Line Arguments
+struct Args {
+    /// A vector containing all the file-paths to read
+    files: Vec<std::path::PathBuf>,
+}
+
+impl Args {
+    /// Parse the command-line arguments
+    fn parse() -> Self {
+        // Get the command line arguments
+        let mut args = std::env::args();
+        args.next(); // Consume the path to the executable
+
+        // Collect all valid file-paths
+        let mut files = Vec::new();
+        while let Some(arg) = args.next() {
+            let path = std::path::PathBuf::from(arg);
+            if std::path::Path::exists(&path) {
+                files.push(path);
+            }
+        }
+
+        // Return self
+        Self { files }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Parse the command line arguments
+    let args = Args::parse();
+    println!("{:#?}", args.files)
 }
