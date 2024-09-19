d2d474a0149437e6d1357c006e271bb2df15411c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-19 15:32:18 +0530

    Implement flag/option parsing

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,6 @@
 // Library
 use crate::colors::{GradientMode, RGB};
+use std::{collections::HashMap, str::FromStr};
 
 /// Command-Line Arguments
 #[derive(Default)]
@@ -31,13 +32,41 @@ impl Args {
         args.next(); // Consume the path to the executable
 
         let mut itself = Args::default();
+        let mut options = HashMap::new();
 
         // Collect all valid file-paths
         while let Some(arg) = args.next() {
+            if arg.starts_with("--") {
+                let key = arg.trim_start_matches("--").to_string();
+                if let Some(value) = args.next() {
+                    options.insert(key, value);
+                } else {
+                    options.insert(key, String::from("true"));
+                }
+                continue;
+            }
+
             let path = std::path::PathBuf::from(arg);
             if std::path::Path::exists(&path) {
                 itself.files.push(path);
             }
+        }
+
+        // Parse options
+        if let Some(start_color) = options.get("start-color") {
+            if let Ok(val) = RGB::from_str(&start_color) {
+                itself.start_color = val;
+            }
+        }
+
+        if let Some(end_color) = options.get("end-color") {
+            if let Ok(val) = RGB::from_str(&end_color) {
+                itself.end_color = val;
+            }
+        }
+
+        if let Some(mode) = options.get("mode") {
+            itself.mode = mode.into();
         }
 
         // Return self
